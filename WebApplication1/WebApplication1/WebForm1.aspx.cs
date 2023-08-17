6f05637ff1c84f17a7c45b2e4514484e9b0b4497
Gurpreet Singh <gurs2806@gmail.com>
2023-08-17 21:03:30 +0530

    firstcommit

---
+++ WebApplication1/WebApplication1/WebForm1.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace WebApplication1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string connetionString;
+            SqlConnection cnn;
+            connetionString = ConfigurationManager.ConnectionStrings["cn"].ConnectionString;
+            cnn = new SqlConnection(connetionString);
+            cnn.Open();
+            Response.Write("Connection Made");
+            cnn.Close();
+        }
+
+        protected void BtnSave_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
